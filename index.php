2479484d0bac2f07cf2127ca134ca0b78793ea83
Leonardus <leonardusdirkstofbergbotes@gmail.com>
2020-04-29 15:03:01 +0200

    created a more modular input controlled method

--- index.php
+++ index.php
@@ -3,34 +3,46 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style> 
-        .wrapper {
-            display: flex;
-            flex-direction: row;
-            flex-wrap: wrap;
-        }
-
-        .product_wrapper {
-            width: 400px;
-            height: 400px;
-            margin: 10px;
-            border: 1px solid grey;
-        }
-
-        .product_wrapper > img {
-            width: 250px;
-            height: 250px;
-        }
-    </style>
+    <title>Online store</title>
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-        <div class="wrapper">
-<?php 
-    include "class.php";
-    $new = new Products();
-    $new->shirt_f();
-?>
+    <div id="wrapper">
+        <form id="select_categ">
+            <label>Category</label><br>
+            <label>Hats M</label>
+            <input type="radio" name="prod_categ" value="hats M"><br>
+            <label>Hats F</label>
+            <input type="radio" name="prod_categ" value="hats F"><br>
+            <label>shirts M</label>
+            <input type="radio" name="prod_categ" value="shirts M"><br>
+            <label>shirts F</label>
+            <input type="radio" name="prod_categ" value="shirts F"><br>
+            <label>Pants M</label>
+            <input type="radio" name="prod_categ" value="pants M"><br>
+            <label>Pants F</label>
+            <input type="radio" name="prod_categ" value="pants F"><br>
+            <label>Shoes M</label>
+            <input type="radio" name="prod_categ" value="shoes M"><br>
+            <label>Shoes F</label>
+            <input type="radio" name="prod_categ" value="shoes F"><br>
+            <button type="submit" onclick="select()"> Select </button>
+        </form>
+
     </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script>
+ function select() {
+     $.ajax({
+         type: 'post',
+         url: 'select_category.php',
+         data: $("#select_categ").serialize(),
+         success: function(data) {
+            $('#wrapper').append(data);
+         }
+     });event.preventDefault(); 
+ }
+   
+</script>
 </body>
 </html>
---
+++ select_category.php
@@ -0,0 +1,9 @@
+<?php 
+    $input = $_POST['prod_categ'];
+    $query = "SELECT * FROM products WHERE prod_categ = \"$input\"";
+
+    include "class/products.php";
+    $new = new Products();
+    $new->select($query);
+
+?>
