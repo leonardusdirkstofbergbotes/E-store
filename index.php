da8a3a612fa41f72969ca1a7db4702188e45db96
Leonardus <leonardusdirkstofbergbotes@gmail.com>
2020-04-29 16:11:21 +0200

    removed the navbar from the main page and added it seperately
    also added more icons

--- index.php
+++ index.php
@@ -7,26 +7,7 @@
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-<form id="select_categ">
-            <label>Category</label><br>
-            <label>Hats M</label>
-            <input type="radio" name="prod_categ" value="hats M"><br>
-            <label>Hats F</label>
-            <input type="radio" name="prod_categ" value="hats F"><br>
-            <label>shirts M</label>
-            <input type="radio" name="prod_categ" value="shirts M"><br>
-            <label>shirts F</label>
-            <input type="radio" name="prod_categ" value="shirts F"><br>
-            <label>Pants M</label>
-            <input type="radio" name="prod_categ" value="pants M"><br>
-            <label>Pants F</label>
-            <input type="radio" name="prod_categ" value="pants F"><br>
-            <label>Shoes M</label>
-            <input type="radio" name="prod_categ" value="shoes M"><br>
-            <label>Shoes F</label>
-            <input type="radio" name="prod_categ" value="shoes F"><br>
-            <button type="submit" onclick="select()"> Select </button>
-        </form>
+<?php include "sections/navbar.php"; ?>
     <div id="wrapper">
         
 
---
+++ sections/navbar.php
@@ -0,0 +1,25 @@
+<nav>
+    <div id="navi">
+    <!-- lOGO COMES HERE --> 
+        <form id="select_categ">
+            <label>Category</label><br>
+            <label>Hats M</label>
+            <input type="radio" name="prod_categ" value="hats M"><br>
+            <label>Hats F</label>
+            <input type="radio" name="prod_categ" value="hats F"><br>
+            <label>shirts M</label>
+            <input type="radio" name="prod_categ" value="shirts M"><br>
+            <label>shirts F</label>
+            <input type="radio" name="prod_categ" value="shirts F"><br>
+            <label>Pants M</label>
+            <input type="radio" name="prod_categ" value="pants M"><br>
+            <label>Pants F</label>
+            <input type="radio" name="prod_categ" value="pants F"><br>
+            <label>Shoes M</label>
+            <input type="radio" name="prod_categ" value="shoes M"><br>
+            <label>Shoes F</label>
+            <input type="radio" name="prod_categ" value="shoes F"><br>
+            <button type="submit" onclick="select()"> Select </button>
+        </form>
+    <div>
+</nav>
