3f4ea461edc6f73f04b5fb064fca82df060ebf4e
Leonardus <leonardusdirkstofbergbotes@gmail.com>
2020-05-01 12:06:30 +0200

    added a div on top of the footer

--- sections/footer.php
+++ sections/footer.php
@@ -1,3 +1,7 @@
+<div id="footy_header">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est officia impedit, ullam quasi eaque sint corporis quisquam vitae obcaecati. Quibusdam sequi sapiente corporis repellat voluptates dolorem odit velit ad deserunt.</p>
+    <div><button>Button</button></div>
+</div>
 <div id="footy">
     <div id="left">
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3294.314033748079!2d19.007320314346202!3d-34.34248498052796!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcdd9dccaf23b01%3A0xd99af027e1e23165!2s21%20Neethling%20St%2C%20Kleinmond%2C%207195!5e0!3m2!1sen!2sza!4v1588096270841!5m2!1sen!2sza" 
