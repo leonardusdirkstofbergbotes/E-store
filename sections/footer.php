aebee784de315e274e429f7796a48f344550ab94
Leonardus <leonardusdirkstofbergbotes@gmail.com>
2020-05-01 10:47:13 +0200

    added a left section to the footer with google maps and follow us links

--- sections/footer.php
+++ sections/footer.php
@@ -1,5 +1,18 @@
 <div id="footy">
     <div id="left">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3294.314033748079!2d19.007320314346202!3d-34.34248498052796!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcdd9dccaf23b01%3A0xd99af027e1e23165!2s21%20Neethling%20St%2C%20Kleinmond%2C%207195!5e0!3m2!1sen!2sza!4v1588096270841!5m2!1sen!2sza" 
+        width="300" height="300" id="maps" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        <div id="stack">
+            <p id="follow">Follow us</p>
+            <a href="#"><img src="./img/icons/facebook.png"> Nate's Clothing</a>
+            <a href="#"><img src="./img/icons/twitter.png"> #Nate'sClothing</a>
+            <a href="#"><img src="./img/icons/email.png"> natesclothing@gmail.com</a>
+            <div id="payment_icons">
+                <img src="./img/icons/visa.png">
+                <img src="./img/icons/paypal.png">
+                <img src="./img/icons/american-express.png">
+            </div>
+        </div>
     </div>
     <div id="right">
     </div>
