a4ac10662525e2be37e8f87545a55b6f8952e089
Leonardus <leonardusdirkstofbergbotes@gmail.com>
2020-05-03 16:23:15 +0200

    fixed a small error

--- index.php
+++ index.php
@@ -14,12 +14,6 @@
     <title>Online store</title>
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script src="./script/functions.js"></script>
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
@@ -50,25 +44,29 @@
     <?php 
         include "sections/footer.php";
     ?>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="./script/functions.js"></script>
         <script>
-        $(document).ready(function() {   
-    var user_id = "<?php echo $_SESSION['user_id']; ?>";
+         
+    var user_id = "<?php if (isset($_SESSION)) {echo $_SESSION['user_id'];} else {echo "none";} ?>";
     console.log(user_id);          
     $.ajax({
         type: 'post',
-        url: 'sections/cart.php',
+        url: 'queries/cart.php',
         data: user_id,
         success: function(data) {
             $("#cart_content").empty();
             $("#cart_content").append(data);
         }
     })
-});
-        function add_to_cart(elmnt) {
+
+    function add_to_cart(elmnt) {
             var id = elmnt.id;
-            var user_id = "<?php echo $_SESSION['user_id']; ?>";
+            var user_id = "<?php if (isset($_SESSION)) {echo $_SESSION['user_id'];} else {echo "none";} ?>";
             $.ajax({
                 type: 'post',
                 url: 'queries/add_cart.php',
@@ -76,7 +74,7 @@
                 success: function() {
                     $.ajax({
                         type: 'post',
-                        url: 'sections/cart.php',
+                        url: 'queries/cart.php',
                         data: user_id,
                         success: function(data) {
                             $("#cart_content").empty();
@@ -87,7 +85,8 @@
             })
         }
 
-    function signupForm(){
+
+        function signupForm(){
 	 $.ajax({
 		type: "POST",
 		url: "queries/create_user.php",
@@ -108,7 +107,7 @@
 }
 
 
-            function select() {
+function select() {
                 $.ajax({
                     type: 'post',
                     url: 'queries/select_category.php',
@@ -120,6 +119,13 @@
                     }
                 });event.preventDefault(); 
             }
+
+        
+
+    
+
+
+            
         
         </script>
 </body>
