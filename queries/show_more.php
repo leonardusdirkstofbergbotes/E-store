88555595957c802f766993b79a1f1394bc529a72
Leonardus <leonardusdirkstofbergbotes@gmail.com>
2020-05-05 10:41:51 +0200

    started the zoom in effect of the img on show_more modal

--- queries/show_more.php
+++ queries/show_more.php
@@ -4,6 +4,13 @@
     $conn = new mysqli('localhost:3308', 'root', '', 'e_store');
 
     $result = $conn->query($get_prod);
-    $data = $result->fetch_assoc();
-
+    while($data = $result->fetch_assoc()) : ?>
+        <div id="modal_content">
+            
+            <div class="img_hover_zoom">
+                <img src="./img/products/<?php echo $data['prod_img']; ?>">
+            </div>
+            
+        </div>
+    <?php endwhile;
 ?>
---
+++ sections/show_more_modal.php
@@ -0,0 +1,19 @@
+
+<!-- Modal -->
+<div id="show_more_modal" class="modal fade bd-example-modal-lg" role="dialog">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content">
+			<div class="modal-header">
+				<a class="close" data-dismiss="modal">X</a>
+				<h3>View more</h3>
+			</div>
+				<div class="modal-body" id="show_more_body">				
+                <!-- content gets rendered in here -->
+				</div>
+				<div class="modal-footer">					
+					<button type="button" class="btn btn-default" data-dismiss="modal" id="close_show_more">Close</button>
+				</div>
+		</div>
+	</div>
+</div>
+    
