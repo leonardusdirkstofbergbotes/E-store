914fb32c6c562bd77791a5a18eb8329d3cde8355
Leonardus <leonardusdirkstofbergbotes@gmail.com>
2020-06-29 11:28:49 +0200

    did media queries on the gallery page and added the footer

--- views/gallery.php
+++ views/gallery.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="../css/styles.css">
 </head>
 <body>
+<img id="to_top" src="../img/icons/back_to_top.png" alt="go back to top">
 <nav>
     <div id="navi">
     <a href="../index.php"><img src="../img/icons/logo.png" id="logo" alt="Nate's Clothing logo"></a>
@@ -129,6 +130,69 @@
 	<?php endwhile; 
 ?>
 
+</div>
+
+<div id="footy_header">
+    <a href="#"><img src="../img/icons/facebook.png" alt="goto facebook"></a>
+    <a href="#"><img src="../img/icons/twitter.png" alt="goto twitter"></a>
+    <a href="#"><img src="../img/icons/email.png" alt="send email"></a>
+</div>
+
+<div id="footy">
+
+    <div id="A"> 
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3294.314033748079!2d19.007320314346202!3d-34.34248498052796!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcdd9dccaf23b01%3A0xd99af027e1e23165!2s21%20Neethling%20St%2C%20Kleinmond%2C%207195!5e0!3m2!1sen!2sza!4v1588096270841!5m2!1sen!2sza" 
+        width="100%" height="100%" id="maps" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>   
+        
+    </div>
+    <!-- A ends -->
+
+    <div id="B">
+        <div id="branch_info">
+            <h3>Visit our branch</h3>
+            <p>Cape Town, South Africa</p>
+            <p>21 Neethling st</p>
+            <p>7430</p>
+        </div>
+        <p></p>
+        <p></p>
+        <div id="trading">
+            <h3>Trading hours</h3>
+            <pre>Mon - Fri:     08:00 - 16:00</pre>
+            <pre>Sat:           09:00 - 13:00</pre>
+            <pre>Sun:           Closed</pre>
+        </div>
+    </div> <!-- B ends -->
+    
+        <form method="post" id="C" enctype="multipart/form-data">
+
+            <h3>Get in touch </h3>
+        
+            
+                <label for='user_name'>Your name </label>
+                <input type="text" class="form-control" maxlength="255" minlength="3" name="user_name" title="">
+            
+
+             
+                <label for='user_email'>Email adress </label>
+                <input type="email" class="form-control" name="user_email" maxlength="128" title="">
+            
+
+             
+                <label for='message'>Your message </label>
+                <textarea class="form-control" name="message" rows="3.5" title=""></textarea>
+            
+
+                <button class="light_pri_button" id="send">Send message</button>
+            
+        </form> <!-- C ends -->
+</div> 
+
+<div id="footer_bottom">
+    <img src="../img/icons/visa.png" alt="pay with Visa">
+    <img src="../img/icons/paypal.png" alt="pay with PayPal">
+    <img src="../img/icons/american-express.png" alt="pay with American Express">
+    <p>Nate's clothing store 2020 </p>
 </div>
 
 
